0149a0e31f586e3876ded192bbd1d3fc490caf31
Aminnn <45661298+aminnoorani@users.noreply.github.com>
2023-05-11 16:57:13 -0400

    Create HiC-Pro.sh

---
+++ HiC-Pro.sh
@@ -0,0 +1,14 @@
+#First the code below should be submitted, after that two separete .sh files will be generated which must be submitted one after another.
+
+module load HiC-Pro/3.1.0 
+
+cd ~/Aminnn/Hi-C
+
+#fastq files could be be in a folder called Colon for example inside another folder called rawdata
+/cluster/tools/software/centos7/hicpro/3.1.0/HiC-Pro_3.1.0/bin/HiC-Pro -i Path/to/fastqs/../rawdata/Colon/ -o path/to/output/HiC_res -c path/to/config-file/that/comes/with/hic-pro/config-hicpro.txt -p
+
+
+#after running first command line, these two files will be generated and they could be submitted step by step
+HiCPro_step1_hic-test_mm10.sh
+HiCPro_step2_hic-test_mm10.sh
+
